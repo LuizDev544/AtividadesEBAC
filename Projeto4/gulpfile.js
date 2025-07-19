7190f48bfd5fedcf899d5544500ac9374d5c0536
Eduardo <eduardoantune12@gmail.com>
2025-07-18 22:25:48 -0300

    Última carta na manga

--- Projeto4/gulpfile.js
+++ Projeto4/gulpfile.js
@@ -1,10 +1,6 @@
 const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
-const gifsicle = require('imagemin-gifsicle');
-const mozjpeg = require('imagemin-mozjpeg').default;
-const pngquant = require('imagemin-pngquant').default;
-const svgo = require('imagemin-svgo').default;
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 function scripts() {
@@ -15,33 +11,18 @@ function scripts() {
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
 function images() {
-    return gulp.src('./src/images/**/*', {
-            encoding: false
-        })
-        .pipe(imagemin([
-            gifsicle({
-                interlaced: true
-            }),
-            mozjpeg({
-                quality: 75,
-                progressive: true
-            }),
-            pngquant({
-                quality: [0.6, 0.8]
-            }),
-            svgo()
-        ]))
+    return gulp.src('./src/images/**/*')
+        .pipe( imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
 
 exports.default = gulp.parallel(styles, images, scripts);
+
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
