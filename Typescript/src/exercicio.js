2f6e8830effc63721e4eb358618d26e4dc9a55e3
Luizfernando <luizfernando.carrupt@gmail.com>
2025-07-29 14:58:25 -0300

    Exercicio do TS

---
+++ Typescript/src/exercicio.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso:
+console.log(multiplicar(5, 3)); // Saída: 15
+console.log(saudacao("Luiz")); // Saída: Olá Luiz
