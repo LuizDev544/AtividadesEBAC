d8182ae99d404546c7e647872f43ab11eab6d441
Luizfernando <luizfernando.carrupt@gmail.com>
2025-08-12 14:00:59 -0300

    Finalizado

--- Projeto4/gulpfile.js
+++ Projeto4/gulpfile.js
@@ -1,37 +1,28 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
+const imagemin = require ('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
-// Minifica scripts
-function scripts() {
+function scripts(){
     return gulp.src('./src/scripts/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'));
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
 }
 
-// Compila SCSS para CSS minificado
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'));
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
 }
 
-// Otimiza imagens
 function images() {
-    return gulp.src('./src/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+    return gulp.src('./src/images/**/*', {encoding: false})
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
 }
 
-// Tarefa padrão — precisa retornar algo para o Gulp saber quando acabou
-exports.default = function () {
-    return gulp.parallel(styles, images, scripts)();
-};
-
-// Observa mudanças nos arquivos
-exports.watch = function () {
+exports.default = gulp.parallel(styles, images,  scripts);
+exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
-    gulp.watch('./src/images/**/*', gulp.parallel(images));
-};
+}
