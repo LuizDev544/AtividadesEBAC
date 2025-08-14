c5aede7ddb96da7e13753682996cd3a01f62ec95
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-08-14 14:36:47 -0300

    Projeto 4 finalizado

--- Projeto4/gulpfile.js
+++ Projeto4/gulpfile.js
@@ -21,7 +21,7 @@ function images() {
     .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images,  scripts);
+exports.default = gulp.series(styles, images,  scripts);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
