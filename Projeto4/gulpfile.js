1f37b213d7f23a59213a831ee583ba4e8ba7dc84
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-08-15 13:12:42 -0300

    Projeto

--- Projeto4/gulpfile.js
+++ Projeto4/gulpfile.js
@@ -23,6 +23,6 @@ function images() {
 
 exports.default = gulp.series(styles, images,  scripts);
 exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./src/scripts/*.js', scripts);
 }
