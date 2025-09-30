734ceb377348d2d9a5726fbe09a820199a5fcafa
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-09-30 14:04:16 -0300

    Teste para o cypress

--- Atividadecypress/cypress/e2e/contacts.cy.js
+++ Atividadecypress/cypress/e2e/contacts.cy.js
@@ -1,49 +1,50 @@
 /// <reference types="cypress" />
 
-describe('Agenda de Contatos - Testes E2E', () => {
+describe('Agenda de Contatos - Testes E2E Adaptáveis', () => {
   beforeEach(() => {
     cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
   })
 
-  it('Deve incluir um novo contato', () => {
-    cy.get('input[placeholder="Nome"]').type('Maria Teste')
-    cy.get('input[placeholder="E-mail"]').type('maria@teste.com')
-    cy.get('input[placeholder="Telefone"]').type('11999999999')
+  // Helper para criar contatos
+  function criarContato(nome, email, telefone) {
+    cy.get('input[placeholder="Nome"]').clear().type(nome)
+    cy.get('input[placeholder="E-mail"]').clear().type(email)
+    cy.get('input[placeholder="Telefone"]').clear().type(telefone)
     cy.contains('Adicionar').click()
+  }
 
-    cy.contains('Maria Teste').should('be.visible')
-    cy.contains('maria@teste.com').should('be.visible')
+  // Helper para realizar ação em um contato (editar ou excluir)
+  function acaoContato(nome, acao) {
+    cy.contains(nome)
+      .closest('li, div, tr') // cobre os principais tipos de container
+      .within(() => {
+        cy.contains(new RegExp(acao, 'i')).click()
+      })
+  }
+
+  it('Deve incluir um novo contato', () => {
+    criarContato('Maria Teste 1', 'maria1@teste.com', '11999990001')
+
+    cy.contains('Maria Teste 1').should('be.visible')
+    cy.contains('maria1@teste.com').should('be.visible')
   })
 
   it('Deve alterar um contato existente', () => {
-    cy.get('input[placeholder="Nome"]').type('Maria Teste')
-    cy.get('input[placeholder="E-mail"]').type('maria@teste.com')
-    cy.get('input[placeholder="Telefone"]').type('11999999999')
-    cy.contains('Adicionar').click()
+    criarContato('Maria Teste 2', 'maria2@teste.com', '11999990002')
 
-    cy.contains('Maria Teste').should('be.visible')
-      .parents('li')
-      .within(() => {
-        cy.contains(/editar/i).should('be.visible').click()
-      })
+    acaoContato('Maria Teste 2', 'editar')
 
     cy.get('input[placeholder="Nome"]').clear().type('Maria Editada')
     cy.contains(/salvar/i).click()
 
     cy.contains('Maria Editada').should('be.visible')
+    cy.contains('Maria Teste 2').should('not.exist')
   })
 
   it('Deve remover um contato', () => {
-    cy.get('input[placeholder="Nome"]').type('Maria Apagar')
-    cy.get('input[placeholder="E-mail"]').type('apagar@teste.com')
-    cy.get('input[placeholder="Telefone"]').type('11999999999')
-    cy.contains('Adicionar').click()
+    criarContato('Maria Apagar', 'apagar@teste.com', '11999990003')
 
-    cy.contains('Maria Apagar').should('be.visible')
-      .parents('li')
-      .within(() => {
-        cy.contains(/excluir/i).should('be.visible').click()
-      })
+    acaoContato('Maria Apagar', 'excluir')
 
     cy.contains('Maria Apagar').should('not.exist')
   })
