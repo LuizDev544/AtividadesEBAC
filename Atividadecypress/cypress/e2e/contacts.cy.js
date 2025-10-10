ee15e1e37c228c9a4ae66d856678b91d8d62a01a
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-10-10 14:08:22 -0300

    Update contacts.cy.js

--- Atividadecypress/cypress/e2e/contacts.cy.js
+++ Atividadecypress/cypress/e2e/contacts.cy.js
@@ -1,57 +1,35 @@
 /// <reference types="cypress" />
 
-describe('Agenda de Contatos - Testes E2E Adaptáveis', () => {
+describe('Testes da Agenda de Contatos', () => {
+  const url = 'https://ebac-agenda-contatos-tan.vercel.app/'
+
   beforeEach(() => {
-    cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
+    cy.visit(url)
   })
-
-  function criarContato(nome, email, telefone) {
-    cy.get('input[placeholder="Nome"]').clear().type(nome)
-    cy.get('input[placeholder="E-mail"]').clear().type(email)
-    cy.get('input[placeholder="Telefone"]').clear().type(telefone)
-    cy.contains('Adicionar').click()
-  }
-
-  function esperarContato(nome) {
-    cy.contains(nome, { timeout: 5000 }).should('be.visible')
-  }
-
-  function acaoContato(nome, acao) {
-    esperarContato(nome)
-
-    cy.contains(nome)
-      .parentsUntil('ul, div, section') 
-      .parent()
-      .within(() => {
-        cy.contains(new RegExp(acao, 'i')).click({ force: true })
-      })
-  }
 
   it('Deve incluir um novo contato', () => {
-    criarContato('Maria Teste 1', 'maria1@teste.com', '11999990001')
-    esperarContato('Maria Teste 1')
-    cy.contains('maria1@teste.com').should('be.visible')
+    cy.get('input[placeholder="Nome"]').type('João Teste')
+    cy.get('input[placeholder="E-mail"]').type('joao@teste.com')
+    cy.get('input[placeholder="Telefone"]').type('11988887777')
+    cy.get('button.adicionar').click()
+    cy.contains('João Teste').should('exist')
+    cy.contains('joao@teste.com').should('exist')
+    cy.contains('11988887777').should('exist')
   })
 
-  it('Deve alterar um contato existente', () => {
-    criarContato('Maria Teste 2', 'maria2@teste.com', '11999990002')
-    esperarContato('Maria Teste 2')
-
-    acaoContato('Maria Teste 2', 'editar')
-
-    cy.get('input[placeholder="Nome"]').clear().type('Maria Editada')
-    cy.contains(/salvar/i).click()
-
-    esperarContato('Maria Editada')
-    cy.contains('Maria Teste 2').should('not.exist')
+  it('Deve editar um contato existente', () => {
+    cy.get('.edit').first().click()
+    cy.get('input[placeholder="Nome"]').clear().type('João Editado')
+    cy.get('input[placeholder="E-mail"]').clear().type('joao_editado@teste.com')
+    cy.get('input[placeholder="Telefone"]').clear().type('11977776666')
+    cy.get('button[type="submit"]').click()
+    cy.contains('João Editado').should('exist')
+    cy.contains('joao_editado@teste.com').should('exist')
+    cy.contains('11977776666').should('exist')
   })
 
   it('Deve remover um contato', () => {
-    criarContato('Maria Apagar', 'apagar@teste.com', '11999990003')
-    esperarContato('Maria Apagar')
-
-    acaoContato('Maria Apagar', 'excluir')
-
-    cy.contains('Maria Apagar', { timeout: 5000 }).should('not.exist')
+    cy.get('.delete').first().click()
+    cy.contains('João Editado').should('not.exist')
   })
 })
