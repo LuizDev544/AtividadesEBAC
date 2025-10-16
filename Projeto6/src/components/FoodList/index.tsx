bbe0328068ccd61f30b7cbbd3bdeb49c4e2b2f5d
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-10-16 16:14:05 -0300

    Vala mds

--- Projeto6/src/components/FoodList/index.tsx
+++ Projeto6/src/components/FoodList/index.tsx
@@ -1,3 +1,30 @@
-import Restaurants from '../../models/Restaurants'
-import Food from '../Food'
-import { List } from './styles'
+import Game from '../../models/Restaurants'
+import Product from '../Food'
+import { Container, List } from './styles'
+
+export type Props = {
+  title: string
+  backgroundColor: 'gray' | 'black'
+  games: Game[]
+}
+
+export const ProductList = ({ title, backgroundColor, games }: Props) => (
+  <Container backgroundColor={backgroundColor}>
+    <div className="container">
+      <h2>{title}</h2>
+      <List>
+        {games.map((game) => (
+          <Product
+            key={game.id}
+            category={game.category}
+            description={game.description}
+            image={game.image}
+            infos={game.infos}
+            system={game.system}
+            title={game.title}
+          />
+        ))}
+      </List>
+    </div>
+  </Container>
+)
