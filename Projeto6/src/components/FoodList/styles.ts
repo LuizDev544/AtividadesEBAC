862ecab4bc4a92feb5c4ff4cd394e77180c1f9cd
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-10-28 14:34:34 -0300

    Update

--- Projeto6/src/components/FoodList/styles.ts
+++ Projeto6/src/components/FoodList/styles.ts
@@ -1,17 +1,10 @@
 import styled from 'styled-components'
-import { Props } from '.'
 import { cores } from '../../styles'
 import { Card } from '../Product/styles'
 
-export const Container = styled.section<Omit<Props, 'title' | 'games'>>`
+export const Container = styled.section`
   padding: 32px 0;
-  background-color: ${(props) =>
-    props.backgroundColor === 'black' ? cores.preta : cores.cinza};
-
-  ${Card} {
-    background-color: ${(props) =>
-      props.backgroundColor === 'black' ? cores.cinza : cores.preta};
-  }
+  background-color: ${cores.begeclaro};
 `
 
 export const List = styled.ul`
