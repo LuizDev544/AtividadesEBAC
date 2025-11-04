b0dbbc081d5d2a810c1d53c2e363f0480f646bb4
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-11-04 14:46:08 -0300

    finalizando o estilo

--- portifolio/portifolio/src/containers/Sobre/index.tsx
+++ portifolio/portifolio/src/containers/Sobre/index.tsx
@@ -1,14 +1,15 @@
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
 
 const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre</Titulo>
-    <p>
+    <Paragrafo tipo="secundario">
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit
       natus in molestias officiis totam amet ullam provident, accusamus
       architecto saepe voluptatem eius numquam? Cum debitis distinctio quidem
       aliquam deserunt beatae.
-    </p>
+    </Paragrafo>
   </section>
 )
 
