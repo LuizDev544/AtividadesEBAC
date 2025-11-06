80fe3c5486733021f43f9e8c76773a038367fc5b
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-11-06 16:34:48 -0300

    uasu

--- portifolio/portifolio/src/containers/SideBar/index.tsx
+++ portifolio/portifolio/src/containers/SideBar/index.tsx
@@ -1,9 +1,19 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/LuizDev544.png" />
-    <Titulo fontSize={20}>Luiz Fernando</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Luiz Fernando</Titulo>
+      <Paragrafo tipo="secundario">Luizdev544</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
