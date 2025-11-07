c7fb3abc8960e534ae0ea9806017413a1dd9e519
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-11-07 14:36:19 -0300

    uau

--- portifolio/portifolio/src/App.tsx
+++ portifolio/portifolio/src/App.tsx
@@ -2,10 +2,12 @@ import Projetos from './containers/Projetos'
 import SideBar from './containers/SideBar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import { ThemeProvider } from 'styled-components'
+import temalight from './themes/light'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temalight}>
       <EstiloGlobal />
       <Container>
         <SideBar />
@@ -14,7 +16,7 @@ function App() {
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ portifolio/portifolio/src/themes/light.ts
@@ -0,0 +1,9 @@
+const temalight = {
+  corPrincipal: '#282a35',
+  corSecundaria: '#949494',
+  corFundo: '#fff',
+  corDeFundoBotao: '#4476bf',
+  cordaBorda: '#c1c1c1'
+}
+
+export default temalight
