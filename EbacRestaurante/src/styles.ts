058fe9a5ec334ce24e609bf2afb33221f259dd93
LuizDev544 <luizfernando.carrupt@gmail.com>
2026-02-10 14:30:04 -0300

    Começando o projeto

---
+++ EbacRestaurante/src/styles.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+const Cores = {
+  Salmao: '#E66767',
+  Branco: '#FFFFFF'
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${Cores.Salmao};
+    color: ${Cores.Branco};
+  }
+`
