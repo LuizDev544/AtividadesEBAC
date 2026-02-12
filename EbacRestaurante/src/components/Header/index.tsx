01dc6395a5ec984bdb3dd9f992f2a5363707ed95
LuizDev544 <luizfernando.carrupt@gmail.com>
2026-02-12 15:00:44 -0300

    Pegando as imagens e criando o cabeçalho

---
+++ EbacRestaurante/src/components/Header/index.tsx
@@ -0,0 +1,5 @@
+import { HeaderImage } from './styles'
+import ImgaemFundo from '../../assets/images/logo.png'
+import Vector from '../../assets/images/Vector.png'
+
+const Header = () => <HeaderImage src={ImgaemFundo} alt="Logo do Restaurante" />
---
+++ EbacRestaurante/src/components/Header/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const HeaderImage = styled.img`
+  background-image: url('../../assets/images/logo.png');
+`
