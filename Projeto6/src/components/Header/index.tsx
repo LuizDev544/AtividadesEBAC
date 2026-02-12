80e238912b86d3b27cfc6e4424bc6aa3fe2d108a
LuizDev544 <luizfernando.carrupt@gmail.com>
2026-02-12 14:36:40 -0300

    criando o cabeçalho

--- Projeto6/src/components/Header/index.tsx
+++ Projeto6/src/components/Header/index.tsx
@@ -1,5 +1,31 @@
-import { HeaderBar } from './styles'
+import { HeaderBar, Links, LinkItem, LinkCart } from './styles'
+import logo from '../../assets/images/logo.svg'
+import carrinho from '../../assets/images/carrinho.svg'
 
-const Header = () => <HeaderBar>cabecalho</HeaderBar>
+const Header = () => (
+  <HeaderBar>
+    <div>
+      <img src={logo} alt="EPLAY" />
+      <nav>
+        <Links>
+          <LinkItem>
+            <a href="#">Categorias</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </nav>
+    </div>
+    <LinkCart>
+      {' '}
+      0 - Produtos(s)
+      <img src={carrinho} alt="Carrinho" />
+    </LinkCart>
+  </HeaderBar>
+)
 
 export default Header
--- Projeto6/src/components/Header/styles.ts
+++ Projeto6/src/components/Header/styles.ts
@@ -5,4 +5,37 @@ export const HeaderBar = styled.header`
   background-color: ${Cores.cinza};
   padding: 24px;
   border-radius: 16px;
+  margimn-bottom: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    color: ${Cores.branca};
+    text-decoration: none;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  img {
+    margin-right: 24px;
+  }
+`
+export const Links = styled.ul`
+  display: flex;
+`
+export const LinkItem = styled.li`
+  margin-right: 16px;
+`
+
+export const LinkCart = styled.a`
+  display: flex;
+
+  img {
+    margin-left: 16px;
+  }
 `
