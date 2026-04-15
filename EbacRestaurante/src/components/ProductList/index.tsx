0b5d0328857663d936d32fb0be7b189c9193c8df
LuizDev544 <luizfernando.carrupt@gmail.com>
2026-04-15 14:08:59 -0300

    Colocando a imagens a mais produtos

--- EbacRestaurante/src/components/ProductList/index.tsx
+++ EbacRestaurante/src/components/ProductList/index.tsx
@@ -1,6 +1,8 @@
 import Product from '../Products'
 import { Section, List } from './styles'
 import estrela from '../../assets/images/estrela.png'
+import SushiImg from '../../assets/images/sushi.png'
+import Massas from '../../assets/images/massas.png'
 
 const ProductList = () => (
   <Section>
@@ -9,7 +11,7 @@ const ProductList = () => (
         <Product
           title="Hioki Sushi"
           description="Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida."
-          image="https://via.placeholder.com/472x217"
+          image={SushiImg}
           infos={['Destaque da semana', 'Japonesa']}
           rating={4.9}
           stars={estrela}
@@ -17,7 +19,39 @@ const ProductList = () => (
         <Product
           title="La Dolce Vita Trattoria"
           description="A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida e sabor inesquecível."
-          image="https://via.placeholder.com/472x217"
+          image={Massas}
+          infos={['Italiana']}
+          rating={4.6}
+          stars={estrela}
+        />
+        <Product
+          title="La Dolce Vita Trattoria"
+          description="A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida e sabor inesquecível."
+          image={Massas}
+          infos={['Italiana']}
+          rating={4.6}
+          stars={estrela}
+        />
+        <Product
+          title="La Dolce Vita Trattoria"
+          description="A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida e sabor inesquecível."
+          image={Massas}
+          infos={['Italiana']}
+          rating={4.6}
+          stars={estrela}
+        />
+        <Product
+          title="La Dolce Vita Trattoria"
+          description="A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida e sabor inesquecível."
+          image={Massas}
+          infos={['Italiana']}
+          rating={4.6}
+          stars={estrela}
+        />
+        <Product
+          title="La Dolce Vita Trattoria"
+          description="A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida e sabor inesquecível."
+          image={Massas}
           infos={['Italiana']}
           rating={4.6}
           stars={estrela}
