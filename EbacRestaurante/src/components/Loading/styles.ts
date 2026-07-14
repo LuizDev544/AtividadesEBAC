645a62be1eb059428cacde85ea83803c6e08038f
LuizDev544 <luizfernando.carrupt@gmail.com>
2026-07-14 14:05:23 -0300

    deletando

--- EbacRestaurante/src/components/Loading/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const LoadingDiv = styled.div`
-  margin: 0;
-  height: 100vh;
-  place-content: center;
-`
--- EbacRestaurante/src/styles/cores.ts
+++
@@ -1,7 +0,0 @@
-export const cores = {
-  vermelho: '#E66767',
-  branco: '#FFFFFF',
-  bege: '#FFEBD9',
-  begeClaro: '#FFF8F2',
-  cinza: '#4B4B4B'
-}
